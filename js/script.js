18d3597f6c8a378bbc39c59842cdc6d2e4fb9dc2
Kinga Palijewska <kinga.jakubowska09@gmail.com>
2023-01-27 16:30:27 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+let body = document.body;
+
+let nextColorName = document.querySelector(".js-nextColorName");
+
+changeBackgroundButton.addEventListener("click", () => {
+    body.classList.toggle("body--grey");
+    nextColorName.innerText = body.classList.contains("body--grey") ? "białe" : "szare";
+});
